25a6987f7bbbf8d50feeafb67bc13d06c13852ba
keerthana <keerthanapradeep0210@gmail.com>
2023-07-03 20:32:52 +0530

    commit

---
+++ Squre.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{
+   int side, Area;
+
+   printf("\nEnter the Length of Side : ");
+   scanf("%d", &side);
+
+   Area = side * side;
+   printf("Area of Square : %d", Area);
+
+}
